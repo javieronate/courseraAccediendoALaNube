fa1aec31192e85126e349fca8ced077ad83212b2
Javier Oñate <jom@dedalo.com.mx>
2016-02-16 12:39:07 -0600

    Initial Commit

---
+++ NuevoOpenLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NuevoOpenLibrary
+//
+//  Created by Javier Oñate Mendía on 2/16/16.
+//  Copyright © 2016 Dédalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
